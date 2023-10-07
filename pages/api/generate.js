85ecf4ae4256b14452f238dc688a050aa17c3bc3
Kenneth Westbrook <kennethwestbrook17@gmail.com>
2023-10-06 20:47:53 -0700

    updates

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -10,46 +10,40 @@ export default async function (req, res) {
     res.status(500).json({
       error: {
         message: "OpenAI API key not configured.",
-      }
+      },
     });
     return;
   }
 
-  const { park, days } = req.body; // Retrieve both park and days from the request body
-  if (park.trim().length === 0 || isNaN(days)) { // Check if days is a valid number
-    res.status(400).json({
-      error: {
-        message: "Please enter a valid national park name and a valid number of days.",
-      }
-    });
-    return;
-  }
+  const { resume, jobDescription } = req.body;
 
   try {
-    // Modify the prompt to ask for an itinerary for the given park and number of days
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(park, days), // Pass both park and days as arguments
-      temperature: 0.6,
-      max_tokens: 500,
+    const coverLetter = await generateCoverLetter(resume, jobDescription);
+    res.status(200).json({ coverLetter });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: {
+        message: "An error occurred during your request.",
+      },
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
-  } catch(error) {
-    if (error.response) {
-      console.error(error.response.status, error.response.data);
-      res.status(error.response.status).json(error.response.data);
-    } else {
-      console.error(`Error with OpenAI API request: ${error.message}`);
-      res.status(500).json({
-        error: {
-          message: 'An error occurred during your request.',
-        }
-      });
-    }
   }
 }
 
-function generatePrompt(park, days) {
-  // Modify the prompt to ask for an itinerary for the specified national park and number of days
-  return `Generate a ${days}-day itinerary for a visit to ${park}.`;
+async function generateCoverLetter(resume, jobDescription) {
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(resume, jobDescription),
+      temperature: 0.6,
+      max_tokens: 500,
+    });
+    return completion.data.choices[0].text;
+  } catch (error) {
+    throw new Error(`Error generating cover letter: ${error.message}`);
+  }
+}
+
+function generatePrompt(resume, jobDescription) {
+  return `Dear Hiring Manager,\n\nI am writing to express my interest in the position of [Job Title] at [Company Name]. With a background in [relevant skills/experience], I am confident in my ability to contribute to your team and help [specific goals of the company].\n\nPlease find attached my resume for your review. I have highlighted key experiences that align with the requirements outlined in the job description. I am particularly excited about the opportunity to [mention specific tasks or projects mentioned in the job description].\n\nThank you for considering my application. I look forward to the possibility of discussing how my skills and experiences align with the needs of [Company Name].\n\nSincerely,\n[Your Name]`;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,79 +1,26 @@
 import React, { useState } from "react";
 import styles from "./index.module.css";
 
-const nationalParks = [
-  { id: 1, name: "Acadia National Park" },
-  { id: 2, name: "American Samoa National Park" },
-  { id: 3, name: "Arches National Park" },
-  { id: 4, name: "Badlands National Park" },
-  { id: 5, name: "Big Bend National Park" },
-  { id: 6, name: "Biscayne National Park" },
-  { id: 7, name: "Black Canyon of the Gunnison National Park" },
-  { id: 8, name: "Bryce Canyon National Park" },
-  { id: 9, name: "Cape Cod National Seashore" },
-  { id: 10, name: "Canyonlands National Park" },
-  { id: 11, name: "Channel Islands National Park" },
-  { id: 12, name: "Congaree National Park" },
-  { id: 13, name: "Cuyahoga Valley National Park" },
-  { id: 14, name: "Denali National Park" },
-  { id: 15, name: "Dry Tortugas National Park" },
-  { id: 16, name: "Everglades National Park" },
-  { id: 17, name: "Glacier National Park" },
-  { id: 18, name: "Grand Canyon National Park" },
-  { id: 19, name: "Grand Teton National Park" },
-  { id: 20, name: "Great Basin National Park" },
-  { id: 21, name: "Great Sand Dunes National Park" },
-  { id: 22, name: "Great Smoky Mountains National Park" },
-  { id: 23, name: "Guadalupe Mountains National Park" },
-  { id: 24, name: "Haleakalā National Park" },
-  { id: 25, name: "Hawaii Volcanoes National Park" },
-  { id: 26, name: "Hot Springs National Park" },
-  { id: 27, name: "Hot Springs National Park" },
-  { id: 28, name: "Isle Royale National Park" },
-  { id: 29, name: "Joshua Tree National Park" },
-  { id: 30, name: "Kings Canyon National Park" },
-  { id: 31, name: "Kobuk Valley National Park" },
-  { id: 32, name: "Lava Beds National Monument" },
-  { id: 33, name: "Mammoth Cave National Park" },
-  { id: 34, name: "Mesa Verde National Park" },
-  { id: 35, name: "Mount Rainier National Park" },
-  { id: 36, name: "North Cascades National Park" },
-  { id: 37, name: "Olympic National Park" },
-  { id: 38, name: "Petrified Forest National Park" },
-  { id: 39, name: "Pictured Rocks National Lakeshore" },
-  { id: 40, name: "Pinnacles National Park" },
-  { id: 41, name: "Redwood National and State Parks" },
-  { id: 42, name: "Rocky Mountain National Park" },
-  { id: 43, name: "Saguaro National Park" },
-  { id: 44, name: "Sequoia and Kings Canyon National Parks" },
-  { id: 45, name: "Shenandoah National Park" },
-  { id: 46, name: "Theodore Roosevelt National Park" },
-  { id: 47, name: "Vicksburg National Military Park" },
-  { id: 48, name: "Voyageurs National Park" },
-  { id: 49, name: "Wind Cave National Park" },
-  { id: 50, name: "Wrangell-St. Elias National Park" },
-  { id: 51, name: "Yellowstone National Park" },
-  { id: 52, name: "Yosemite National Park" },
-  { id: 53, name: "Zion National Park" },
-];
-
-
 export default function Home() {
-  const [parkInput, setParkInput] = useState("");
-  const [daysInput, setDaysInput] = useState("");
-  const [result, setResult] = useState("");
-   const [savedTrips, setSavedTrips] = useState([]);
+  const [resumeInput, setResumeInput] = useState(""); // Add state for resume input
+  const [jobDescription, setJobDescription] = useState("");
+  const [coverLetter, setCoverLetter] = useState("");
 
-
-  async function onSubmit(event) {
+  async function onCoverLetterGenerate(event) {
     event.preventDefault();
     try {
-      const response = await fetch("/api/generate", {
+      if (!resumeInput) {
+        alert("Please provide a resume.");
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append("resume", resumeInput);
+      formData.append("jobDescription", jobDescription);
+
+      const response = await fetch("/api/generate-cover-letter", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ park: parkInput, days: daysInput }),
+        body: formData,
       });
 
       const data = await response.json();
@@ -81,83 +28,38 @@ export default function Home() {
         throw data.error || new Error(`Request failed with status ${response.status}`);
       }
 
-      setResult(data.result);
-      setParkInput("");
-      setDaysInput("");
+      setCoverLetter(data.coverLetter);
     } catch (error) {
       console.error(error);
       alert(error.message);
     }
   }
 
-  // Function to handle saving the trip
-  function saveTrip() {
-    if (result) {
-      const newSavedTrips = [...savedTrips, result];
-      setSavedTrips(newSavedTrips);
-      // Save the updated list of trips to local storage
-      localStorage.setItem("savedTrips", JSON.stringify(newSavedTrips));
-    }
-  }
-
-  // Function to handle clearing the current trip
-  function clearTrip() {
-    setResult("");
-  }
-
-  // Function to handle displaying saved trips
-  function showSavedTrips() {
-    const savedTrips = JSON.parse(localStorage.getItem("savedTrips")) || [];
-    if (savedTrips.length === 0) {
-      alert("No saved trips yet.");
-    } else {
-      const savedTripsText = savedTrips.join("\n\n");
-      alert("Saved Trips:\n\n" + savedTripsText);
-    }
-  }
-
-
   return (
     <div>
       <main className={styles.main}>
-        <h3>Generate National Park Itinerary</h3>
-        <form onSubmit={onSubmit}>
-          <label htmlFor="park">Select a national park:</label>
-          <select
-            name="park"
-            value={parkInput}
-            onChange={(e) => setParkInput(e.target.value)}
-          >
-            <option value="">Select a national park</option>
-            {nationalParks.map((park) => (
-              <option key={park.id} value={park.name}>
-                {park.name}
-              </option>
-            ))}
-          </select>
-          <br />
-          <label htmlFor="days">Number of Days:</label>
-          <input
-            type="text"
-            name="days"
-            placeholder="Number of Days"
-            value={daysInput}
-            onChange={(e) => setDaysInput(e.target.value)}
-          />
-          <br />
-          <input type="submit" value="Generate itinerary" />
-        </form>
-        <div className={styles.result}>{result}</div>
-        <div className={styles.result}>{result}</div>
-        <button onClick={saveTrip}>Save Trip</button>
-        <button onClick={clearTrip}>Clear</button>
+        <h3>Generate Cover Letter</h3>
+        <label htmlFor="resume">Provide your resume:</label>
+        <textarea
+          name="resumeInput"
+          rows="8"
+          cols="50"
+          value={resumeInput}
+          onChange={(e) => setResumeInput(e.target.value)}
+        ></textarea>
+        <br />
+        <label htmlFor="jobDescription">Enter the job description:</label>
+        <textarea
+          name="jobDescription"
+          rows="4"
+          cols="50"
+          value={jobDescription}
+          onChange={(e) => setJobDescription(e.target.value)}
+        ></textarea>
+        <br />
+        <button onClick={onCoverLetterGenerate}>Generate Cover Letter</button>
+        <div className={styles.result}>{coverLetter}</div>
       </main>
-       <button
-        className={styles.myTripsButton}
-        onClick={showSavedTrips}
-      >
-        My Trips
-      </button>
     </div>
   );
 }
